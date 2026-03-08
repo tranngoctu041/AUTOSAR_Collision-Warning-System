667ce51b110f3cbe6371895ff7384058f23ea1bf
tranngoctu041 <taitutuan041@gmail.com>
2026-03-08 15:41:02 +0700

    mã nguồn test CAN transmit

--- testCAN_S32K/Debug_FLASH/board/peripherals_osif_1.d
+++
@@ -1,9 +0,0 @@
-board/peripherals_osif_1.o: ../board/peripherals_osif_1.c \
- ../board/peripherals_osif_1.h ../SDK/rtos/osif/osif.h \
- C:/Users/Admin/Desktop/DATN/DATN/testCAN_S32K/SDK/platform/devices/status.h
-
-../board/peripherals_osif_1.h:
-
-../SDK/rtos/osif/osif.h:
-
-C:/Users/Admin/Desktop/DATN/DATN/testCAN_S32K/SDK/platform/devices/status.h:
